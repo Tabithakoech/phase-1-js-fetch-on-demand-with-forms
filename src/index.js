d8fdb1a1eab907883e4997741dedde027639ec8f
TabithaKoech <tabbyk1718@gmail.com>
2022-06-15 14:51:23 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
+// callback function init
 const init = () => {
-  
-}
+    const inputForm = document.querySelector('form')
+    inputForm.addEventListener('submit', (event) => {
+        e.preventDefault()// stop page from  refreshing
 
+        const input = document.querySelector('input#searchByID');
+        fetch(`http://localhost:3000/movies/${input.value}`)
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+        })
+    })
+}
+// ensuring the JavaScript we write executes when the DOM is fully loaded.
 document.addEventListener('DOMContentLoaded', init);
